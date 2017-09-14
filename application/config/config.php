2a6301ccc18f2f8f18d4941c72381611933410b2
demaram <ramlidennis@gmail.com>
2017-09-15 02:23:55 +0700

    class name fixed

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/sikad';
+$root = 'http' . ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') ? 's' : '') .'://'.$_SERVER['HTTP_HOST'].str_replace('//','/',dirname($_SERVER['SCRIPT_NAME']).'/');
 
+$config['base_url'] = $root;
 /*
 |--------------------------------------------------------------------------
 | Index File
